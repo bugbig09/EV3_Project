6646a7e4329efd01a4b348b1b8bfc4a328a64841
Marian Rohlfing <marianrrrr@gmx.net>
2018-04-11 14:09:38 +0200

    Test Prog

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.Brick;
 
-public class Main {
+ public class Main {
 
-}
+ public static void main (String args[]){
+ //Löschen vorheriger Anzeigen auf dem LCD
+ LCD.clear();
+ //Ausgeben des Strings auf de, LCD in Spalte 0 und Zeile 5
+ LCD.drawString("Hallo Welt", 0, 5);
+ //Warten, bis irgendeine Taste gedrückt wird
+ Button.waitForAnyPress();
+ //Löschen und Aktualisieren der Anzeige,
+ //nachdem eine Taste gedrückt wurde
+ LCD.clear();
+ LCD.refresh();
+ }
+
+ }

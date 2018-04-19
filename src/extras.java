a688253174da5a5f4fc6c2f2b7b0cfb3dc632aaa
Marian Rohlfing <marianrrrr@gmx.net>
2018-04-19 07:55:49 +0200

    pid erstellt

--- src/extras.java
+++ src/extras.java
@@ -14,31 +14,4 @@ public class extras {
 			return(output);
 		}
 	}
-	
-	/*
-	Kp = 1000				! REMEMBER we are using Kp*100 so this is really 10
-	Ki = 100				! REMEMBER we are using Ki*100 so this is really 1 !
-	Kd = 10000 				! REMEMBER we are using Kd*100 so this is really 100!
-	offset = 45	
-	Tp = 50 				! Initialize the variables
-	integral = 0 			! the place where we will store our integral
-	lastError = 0 			! the place where we will store the last error value
-	derivative = 0 			! the place where we will store the derivative
-	
-Loop forever	
-	LightValue = read light sensor					! what is the current light reading?
-	error = LightValue - offset						! calculate the error by subtracting the offset 
-	integral = integral + error						! calculate the integral
-	derivative = error - lastError					! calculate the derivative
-	Turn = Kp*error + Ki*integral + Kd*derivative 	! the "P term" the "I term" and the "D term"
-	Turn = Turn/100									! Remember to undo the affect of the factor of 100 in Kp, Ki, Kd
-	powerA = Tp + Turn								! Power Motor A
-	powerC = Tp - Turn								! Power Motor C
-	MOTOR A direction=forward power=PowerA 			! actually issue the command in a MOTOR block
-	MOTOR C direction=forward power=PowerC 			! same for the other motor but using the other power level 
-	lastError = error 								! save the current error so it can be the lastError next time around
-end loop forever                      				! done with loop, go back and do it again.
-	 */
-	
-
 }
--- src/pid.java
+++ src/pid.java
@@ -1,7 +1,26 @@
 
 public class pid {
-
-	short PID(short Pwert, short Iwert, short Dwert, short offset, short input) {
-		
+	
+	/**
+	 * Pwert 1000 = 10
+	 * Iwert 100 = 1		Werte werden durch 100 geteilt
+	 * Dwert 10000 = 100
+	 */
+	
+	
+	int integral = 0;
+	int lastError = 0;
+	int derivative = 0;
+	int error;
+	int output;
+	
+	int PID(int Pwert, int Iwert, int Dwert, int offset, int input) {
+		error = (input - offset);
+		integral = (integral + error);
+		derivative = (error - lastError);
+		output = ((Pwert*error) + (Iwert*integral) + (Dwert*derivative));
+		output = (output/100);
+		lastError = error;
+		return(output);
 	}
 }
